73509c9250387bf51c9a59cddc16353dd166dedf
Syed Faraz Abrar <faraz.abrar9@gmail.com>
2019-07-27 20:29:14 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+A simple pwntools skeleton script that I use for all of my binary exploit scripts.
+
+Credits goes to [tcode2k16](https://github.com/tcode2k16)!
---
+++ skeleton.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python2
+
+from pwn import *
+import sys
+
+argv = sys.argv
+
+DEBUG = False
+BINARY = './binary'
+
+context.binary = BINARY
+context.terminal = ['tmux', 'splitw', '-v']
+
+if context.bits == 64:
+  r = process(['ROPgadget', '--binary', BINARY])
+  gadgets = r.recvall().strip().split('\n')[2:-2]
+  gadgets = map(lambda x: x.split(' : '),gadgets)
+  gadgets = map(lambda x: (int(x[0],16),x[1]),gadgets)
+  r.close()
+
+  pop_rdi = 0
+  pop_rsi_r15 = 0
+  pop_rdx = 0
+
+  for addr, name in gadgets:
+    if 'pop rdi ; ret' in name:
+      pop_rdi = addr
+    if 'pop rsi ; pop r15 ; ret' in name:
+      pop_rsi_r15 = addr
+    if 'pop rdx ; ret' in name:
+      pop_rdx = addr
+
+  def call(f, a1, a2, a3):
+    out = ''
+    if a1 != None:
+      out += p64(pop_rdi)+p64(a1)
+    if a2 != None:
+      out += p64(pop_rsi_r15)+p64(a2)*2
+    if a3 != None:
+      if pop_rdx == 0:
+        print 'RDX GADGET NOT FOUND'
+        exit(-1)
+      else:
+        out += p64(rdx)+p64(a3)
+    return out+p64(f)
+
+def attach_gdb():
+  gdb.attach(sh)
+
+if DEBUG:
+  context.log_level = 'debug'
+
+if len(argv) < 2:
+  stdout = process.PTY
+  stdin = process.PTY
+
+  sh = process(BINARY, stdout=stdout, stdin=stdin)
+
+if DEBUG:
+  attach_gdb()
+
+  REMOTE = False
+else:
+  sh = remote('pwn.hsctf.com', 3333)
+  REMOTE = True
